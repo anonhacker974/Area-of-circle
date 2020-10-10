cae0bbfc40acb71ac03c594b92be706ee231f275
anonhacker974 <45949823+anonhacker974@users.noreply.github.com>
2020-10-10 10:47:04 +0530

    Create freqOfChar.py

---
+++ freqOfChar.py
@@ -0,0 +1,12 @@
+test_str = "Hello World"
+
+all_freq = {} 
+
+for i in test_str: 
+	if i in all_freq: 
+		all_freq[i] += 1
+	else: 
+		all_freq[i] = 1
+
+print ("Count of all characters is :\n "
+										+ str(all_freq)) 
